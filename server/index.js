35bdb034ef0d8f39aff9c5dfdfc9ced0d1c6de84
Sandesh Subedi <subedisandesh0123@gmail.com>
2023-10-29 18:25:42 +0545

    Api Socket Changes

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import { Server } from "socket.io";
 import mongoose from 'mongoose';
 import { SECRET_STRING } from './connect.js';
 import chatRoute from './routes/chatRoute.js';
@@ -19,3 +20,42 @@ app.listen(5000, (req, res) => {
 mongoose.connect(SECRET_STRING)
     .then(() => console.log("MongoDB connection established"))
     .catch((error) => console.log("MongoDB connection failed :", error.message))
+
+
+// SOCKET SETUP
+
+const io = new Server({ cors: "http://localhost:5173", cors: "https://safebook.vercel.app/" });
+
+let onlineUsers = [];
+
+io.on("connection", (socket) => {
+    console.log("New connection", socket.id);
+
+    socket.on("addNewUser", (userId) => {
+        !onlineUsers.some(user => user.userId === userId) &&
+            onlineUsers.push({
+                userId,
+                socketId: socket.id
+            })
+        // console.log("onlineUsers", onlineUsers);
+
+        io.emit("getOnlineUsers", onlineUsers);
+    })
+
+    // add message
+    socket.on("sendMessage", (message) => {
+        const user = onlineUsers.find((user) => user.userId == message.recepientId);
+        if (user) {
+            io.to(user.socketId).emit("getMessage", message);
+        }
+    })
+
+    socket.on("disconnect", () => {
+        onlineUsers = onlineUsers.filter(user => user.socketId !== socket.id);
+
+        io.emit("getOnlineUsers", onlineUsers);
+    })
+});
+
+
+io.listen(3000);
